2101d5776d7ad614ea7d04c7d1f770371d5fadb0
Toshiaki Maki <makingx@gmail.com>
2024-10-15 22:50:41 +0900

    Add plan

--- src/main/java/com/example/awsservicebroker/servicebroker/AwsService.java
+++ src/main/java/com/example/awsservicebroker/servicebroker/AwsService.java
@@ -1,21 +1,32 @@
 package com.example.awsservicebroker.servicebroker;
 
+import java.util.List;
+
 import com.example.awsservicebroker.utils.StringUtils;
 
 import org.springframework.lang.Nullable;
 
 public enum AwsService {
 
-	IAM_ROLE("5edee818-720e-499e-bf10-55dfae43703b"), S3("2de9054b-fad9-4b25-84c9-efe432b14c2f");
+	IAM_ROLE("5edee818-720e-499e-bf10-55dfae43703b",
+			List.of(new SimplePlan("free", "0ed05edb-7e48-4ad9-bded-8fe37638e2e3"))),
+	S3("2de9054b-fad9-4b25-84c9-efe432b14c2f", List.of(new SimplePlan("free", "a42e2c0c-64e0-41c5-a59c-be52c592812e")));
 
 	private final String serviceId;
 
-	AwsService(String serviceId) {
+	private final List<Plan> plans;
+
+	AwsService(String serviceId, List<Plan> plans) {
 		this.serviceId = serviceId;
+		this.plans = plans;
 	}
 
 	public String serviceId() {
 		return serviceId;
+	}
+
+	public List<Plan> plans() {
+		return plans;
 	}
 
 	@Nullable
@@ -30,6 +41,18 @@ public enum AwsService {
 
 	public String getServiceBrokerServiceBeanName() {
 		return StringUtils.toUpperCamel(name()) + "ServiceBrokerService";
+	}
+
+	public interface Plan {
+
+		String planName();
+
+		String planId();
+
+	}
+
+	record SimplePlan(String planName, String planId) implements Plan {
+
 	}
 
 }
